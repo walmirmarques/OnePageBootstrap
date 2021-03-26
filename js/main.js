aef64aae41ae19573dfa3499feb5772d69e9a676
Walmir marques Filho <walmir-mf@hotmail.com>
2021-03-26 16:32:28 -0300

    Primeiro commit! Criação de uma Onepage usando bootstrap 4

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function($){
+    "use strict";
+    $('.contar').counterUp({
+        time:1000,
+        delay:10
+    });
+
+var $doc = $('html,body');
+$('.scroll-page').click(function(){
+    $doc.animate({
+        scrollTop: $($.attr(this,'href')).offset().top
+    }, 500);
+    return false;
+})
+
+})(jQuery);
